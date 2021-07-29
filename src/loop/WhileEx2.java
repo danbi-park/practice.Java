5acf8af0c43e4beb34f49116b24c0e6e6a929afd
sweg-db <bhd03067@naver.com>
2021-07-29 18:00:17 +0900

    210729 Shuffle, 배열, 2차원배열

---
+++ src/loop/WhileEx2.java
@@ -0,0 +1,14 @@
+package loop;
+
+public class WhileEx2 {
+    public static void main(String[] args) {
+        int sum =0;
+        int i = 0;
+        while (true) {
+            System.out.printf("%d - %d%n",i,sum);
+            sum += ++i;
+            if(sum >= 100) break;
+        }
+        System.out.println("sum: " + sum);
+    }
+}
