b71399ccc330d503f3b483bc71557a596bf4da99
sweg-db <bhd03067@naver.com>
2021-08-18 18:51:12 +0900

    8월 18일 TcpIpMultichatClient 수정

--- src/main/network/TcpIpMultichatClient.java
+++ src/main/network/TcpIpMultichatClient.java
@@ -1,68 +1,80 @@
 package network;
 
+import sun.plugin.javascript.JSClassLoader;
+import swing.BasicFrm;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
-public class TcpIpMultichatClient {
-    public static void main(String[] args) {
-        String nickName = JOptionPane.showInputDialog("채팅할 대화명을 입력하세요. :)");
-        String ip = JOptionPane.showInputDialog("서버 IP를 입력하세요.");
+public class TcpIpMultichatClient extends BasicFrm{
+    private JTextArea ta;
+    private JScrollPane scp;
+    private JTextField tf;
+    private String nickName;
+    private DataOutputStream out;
+
+
+    public TcpIpMultichatClient() {
+        super(500, 600, "Chatting room");
+        tf.requestFocus();
+    }
+
+    @Override
+    public void init() {
+        nickName = JOptionPane.showInputDialog("채팅할 대화명을 입력하세요. :)");
+        String ip = JOptionPane.showInputDialog("서버 IP를 입력하세요.","192.168.0.127"); //쌤 서버
+
+        ta = new JTextArea();
+        ta.setEditable(false); //수정할 수 없도록
+        scp = new JScrollPane(ta);
+        tf = new JTextField();
+        ta.setFont(new Font("맑은고딕",Font.PLAIN,20));
+        tf.setFont(new Font("맑은고딕",Font.PLAIN,20));
+
         try {
             Socket socket = new Socket(ip,7777);
-            System.out.println("서버에 접속되었습니다.");
-            //1.main.thread 돌리기 쉬워요 :>
-            //2.sender와 receiver 두줄만 적어주면 돼요
-            Sender sender = new Sender(socket,nickName); //6.매개변수로 socket과 nickname을 넣어주면 생성자에서 초기화됨
-            sender.start();
+            out = new DataOutputStream(socket.getOutputStream());
+            out.writeUTF(nickName);
             Receiver receiver = new Receiver(socket);
             receiver.start();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-    }
-
-    //3.
-    //main에 static붙어서 sender를 쓰려면 static을 붙여줘야한다.
-    //thread클래스를 상속받아야 함
-    //Thread에서 run메서드를 재정의 해줘야 함
-    static class Sender extends Thread{
-        Socket socket;  //네트워크를 사용하기 위한 통로
-
-        DataOutputStream output; //4.출력 스트림(socket으로 부터 획득 그래야 해당 네트워크로 보낼 수 있음)
-        String nickName; //통신할 때 구분되는 id같은 개념
-
-//5.위의 변수들을 아래 생성자로 초기화 시켜 Thread로 보냄
-        public Sender(Socket socket, String nickName){
-            this.socket = socket;
-            this.nickName = nickName;
-            try {
-                //소켓으로부터 출력스트림을 획득
-                output = new DataOutputStream(socket.getOutputStream());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        @Override
-        public void run() {
-            try {
-                if(output != null) output.writeUTF(nickName);
-                while (output != null) {
-                output.writeUTF("["+nickName+"]"
-                        + JOptionPane.showInputDialog("메시지를 입력하세요")); //한글로 보낼 때 깨지지말라고 utf
+        tf.addActionListener(e -> {
+                String msg = tf.getText();
+                if(msg == null || msg.equals("")) return;
+//                if(msg.equalsIgnoreCase("Q")){
+//                    JOptionPane.showMessageDialog(null,"Chatting is terminated");
+//                    System.exit(-1);
+//                }
+                try {
+                    out.writeUTF("[" + nickName + "]" + msg);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+                tf.setText("");
+        });
+    }
+
+    @Override
+    public void arrange() {
+        add(scp, "Center");
+        add(tf, "South");
+    }
+
+    public static void main(String[] args) {
+        new TcpIpMultichatClient();
     }
 
 
-    static class Receiver extends Thread{
+    class Receiver extends Thread{
         Socket socket;
         DataInputStream input;
         //생성자
@@ -79,14 +91,13 @@ public class TcpIpMultichatClient {
         public void run() {
             while (input != null) { //객체가 입력받아야지만 돌아감
                 try {
-                    System.out.println( input.readUTF());
+//                    System.out.println( input.readUTF());
+                    ta.append(input.readUTF() + "\n");
+                    ta.setCaretPosition(ta.getDocument().getLength());
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
-
     }
-
-
 }
--- src/main/network/TcpIpMultichatServer.java
+++ src/main/network/TcpIpMultichatServer.java
@@ -5,79 +5,79 @@ import java.io.*;
 import java.util.*;
 
 public class TcpIpMultichatServer {
-	HashMap clients;
-	
-	TcpIpMultichatServer() {
-		clients = new HashMap();
-		Collections.synchronizedMap(clients); //동기화
-	}
+   HashMap clients;
 
-	public void start() {
-		ServerSocket serverSocket = null;
-		Socket socket = null;
+   TcpIpMultichatServer() {
+      clients = new HashMap();
+      Collections.synchronizedMap(clients);
+   }
 
-		try {
-			serverSocket = new ServerSocket(7777);
-			System.out.println("서버가 시작되었습니다.");
+   public void start() {
+      ServerSocket serverSocket = null;
+      Socket socket = null;
 
-			while(true) {
-				socket = serverSocket.accept();
-				System.out.println("["+socket.getInetAddress() + ":"+socket.getPort()+"]"+"에서 접속하였습니다.");
-				ServerReceiver thread = new ServerReceiver(socket);
-				thread.start();
-			}
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	} // start()
+      try {
+         serverSocket = new ServerSocket(7777);
+         System.out.println("Server started...");
 
-	void sendToAll(String msg) {
-		Iterator it = clients.keySet().iterator();
-		
-		while(it.hasNext()) {
-			try {
-				DataOutputStream out = (DataOutputStream)clients.get(it.next());
-				out.writeUTF(msg);
-			} catch(IOException e){}
-		} // while
-	} // sendToAll
+         while(true) {
+            socket = serverSocket.accept();
+            System.out.println("Connected from ["+socket.getInetAddress() + ":"+socket.getPort()+"]");
+            ServerReceiver thread = new ServerReceiver(socket);
+            thread.start();
+         }
+      } catch(Exception e) {
+         e.printStackTrace();
+      }
+   } // start()
 
-	public static void main(String args[]) {
-		new TcpIpMultichatServer().start();
-	} 
+   void sendToAll(String msg) {
+      Iterator it = clients.keySet().iterator();
 
-	class ServerReceiver extends Thread {
-		Socket socket;
-		DataInputStream in;
-		DataOutputStream out;
+      while(it.hasNext()) {
+         try {
+            DataOutputStream out = (DataOutputStream)clients.get(it.next());
+            out.writeUTF(msg);
+         } catch(IOException e){}
+      } // while
+   } // sendToAll
 
-		ServerReceiver(Socket socket) {
-			this.socket = socket;
-			try {
-				in = new DataInputStream(socket.getInputStream());
-				out = new DataOutputStream(socket.getOutputStream());
-			} catch(IOException e) {}
-		}
+   public static void main(String args[]) {
+      new TcpIpMultichatServer().start();
+   }
 
-		public void run() {
-			String name = "";
-			try {
-				name = in.readUTF();
-				sendToAll("#"+name+"님이 들어오셨습니다.");
+   class ServerReceiver extends Thread {
+      Socket socket;
+      DataInputStream in;
+      DataOutputStream out;
 
-				clients.put(name, out);
-				System.out.println("현재 서버접속자 수는 " + clients.size() + "입니다.");
-				while(in!=null) {
-					sendToAll(in.readUTF());
-				}
-			} catch(IOException e) {
-				// ignore
-			} finally {
-				sendToAll("#"+name+"님이 나가셨습니다.");
-				clients.remove(name);
-				System.out.println("["+socket.getInetAddress()+":"+socket.getPort()+"]"+"에서 접속을 종료하였습니다.");
-				System.out.println("현재 서버접속자 수는 "+ clients.size()+"입니다.");
-			} // try
-		} // run
-	} // ReceiverThread
+      ServerReceiver(Socket socket) {
+         this.socket = socket;
+         try {
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+         } catch(IOException e) {}
+      }
+
+      public void run() {
+         String name = "";
+         try {
+            name = in.readUTF();
+            sendToAll("#"+name+" Entered");
+
+            clients.put(name, out);
+            System.out.println("Now Number of Connector is " + clients.size());
+            while(in!=null) {
+               sendToAll(in.readUTF());
+            }
+         } catch(IOException e) {
+            // ignore
+         } finally {
+            sendToAll("#"+name+"is disconnected");
+            clients.remove(name);
+            System.out.println("["+socket.getInetAddress()+":"+socket.getPort()+"]"+"is terminated");
+            System.out.println("Now Number of Connector is " + clients.size());
+         } // try
+      } // run
+   } // ReceiverThread
 } // class
